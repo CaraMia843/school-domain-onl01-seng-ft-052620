7d94333a04ce1c2c44b956134f7c2fb2b85d9ef9
Cara Zellner <carajeanine@gmail.com>
2020-06-05 19:37:22 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
-# code here!
+class School
+  attr_accessor :roster, :name, :grade
+  attr_reader :School
+  
+  def initialize(school)
+    @school = School
+  end
+  
+  def roster
+    roster = {}
+  end
+  
+  def add_student(name,grade)
+    roster[grade] = []
+    roster[grade] << name
+  end
+  
+  def grade(grade_level)
+    roster[grade_level]
+  end
+  
+  def sort 
+    sorted = {} 
+    roster.each do |grade, students|
+      sorted[grade] = students.sort
+    end
+  end
+end
